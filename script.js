dbcb4a1f3c6906814709b069c83bb0ede8bcd43c
paryan <pryntl2007@gmail.com>
2024-08-10 14:35:36 +0330

    Update [java

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
+//the function base code.
 function lastCharacter(word) {
+  //made a variable to show the final result.
   let output = 0;
+  //if our word is string, we can simply use this method to show the last letter of the word.(we minus one cause the actual lenght starts from 0 instead of 1. 
   if (typeof word == "string") {
     output = word[word.length - 1];
-  } else {
+  } 
+  //if the word isn't a string, then it doesn't have any last letter for us to show.
+  else {
     output = "the string has zero length";
   }
+  //at the end returning the main var.
   return output;
 }
+//testing.
 console.log(lastCharacter("hehehe"));
 console.log(lastCharacter("hahaha"));
